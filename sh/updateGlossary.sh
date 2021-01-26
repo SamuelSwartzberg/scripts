9ccc6328270f2aa611fbfd70f73f00a06fc608b3
SamuelSwartzberg <me@samswartzberg.com>
2021-01-26 12:59:20 +0100

    ctd

--- sh/updateGlossary.sh
+++ sh/updateGlossary.sh
@@ -1,5 +1,3 @@
-key="$1"
-value="$2"
-echo "$1	$2" >> /Users/sam/.code-glossary.tsv
-result=`d8 ../update-code-settings.js -- "$1" `
+sort --ignore-case -o /Users/sam/.code-glossary.tsv  /Users/sam/.code-glossary.tsv
+result=`d8 ../update-code-settings.js`
 echo "$result" > "/Users/sam/Library/Application Support/Code/User/settings.json"
