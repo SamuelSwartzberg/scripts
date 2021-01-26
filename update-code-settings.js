f28233884d97aa1f8ffd7875ababe1e8f3d00885
SamuelSwartzberg <me@samswartzberg.com>
2021-01-26 12:58:32 +0100

    improve handling of autoglossary

--- update-code-settings.js
+++ update-code-settings.js
@@ -1,5 +1,12 @@
-key=arguments[0];
 settingsJsonString = read("/Users/sam/Library/Application Support/Code/User/settings.json")
+glossaryTSVData = read("/Users/sam/.code-glossary.tsv");
+glossaryArray = glossaryTSVData.split("\n").map(item=>item.split("\t")[0]);
 settingsObject = JSON.parse(settingsJsonString);
-settingsObject["todohighlight.keywords"].push(key);
+glossaryArray.sort((item1, item2)=>{
+  let item1amountOfWords = item1.split(" ").length;
+  let item2amountOfWords = item2.split(" ").length;
+  if (item1amountOfWords >= item2amountOfWords) return -1;
+  else return 1;
+})
+settingsObject["todohighlight.keywords"] = glossaryArray;
 console.log(JSON.stringify(settingsObject));
