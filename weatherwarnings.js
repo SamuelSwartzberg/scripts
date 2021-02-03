bb1c7e89c974fa2b20c3904f40699d0218fac3b6
SamuelSwartzberg <me@samswartzberg.com>
2021-02-03 21:37:14 +0100

    add weather warning retrieval script

---
+++ weatherwarnings.js
@@ -0,0 +1,17 @@
+let weatherwarnings = read("/Users/sam/tempweatherwarnings.json");
+let weatherwarningscleaned = weatherwarnings.replace("warnWetter.loadWarnings(", "").replace(");", "");
+let parsedwarnings = JSON.parse(weatherwarningscleaned);
+let munichwarnings = Object.values(parsedwarnings.warnings).filter(item =>{
+  firstChildItem = item[0];
+  if (firstChildItem.regionName.includes("München")) return true;
+})
+console.log(munichwarnings[0][0].regionName + "\n")
+  for (let subwarning of munichwarnings[0]){
+      let timelengthstr = new Date(subwarning.start).toLocaleTimeString().replace(":00 PM", " PM") + ` (${Math.round((subwarning.end-subwarning.start)/(1000*60*60))}h)`;
+      console.log(subwarning.event+ "\n")
+      console.log("Level: " + subwarning.level + " - " + timelengthstr + "\n")
+      console.log(subwarning.description)
+      console.log("\n\n")
+    
+
+}
